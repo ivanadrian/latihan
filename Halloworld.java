a08815c6d4d1b63f4f620d0897ff0d7dd97bc746
vhan14 <ivanadrian1404@gmail.com>
2015-09-12 16:52:05 +0700

    Halloworld.java

---
+++ Halloworld.java
@@ -0,0 +1,15 @@
+/**
+* file : Halloworld.java
+* kelas : Halloworld
+*
+* deskripsi : sebuah aplikasi untuk menampilkan "Halo Dunia!!!" ke dalam standar keluaran
+*
+**/
+public class Halloworld{
+		//method main, hanya kelas ang memiliki method
+		//main yang dapat dijalankan
+		public static void main (String[] args){
+				//menampilkan tulisan "Hallo Dunia!!!"
+				System.out.println("Hallo Dunia!!!");
+		}
+}
